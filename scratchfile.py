dcb978394f728a1d0a32c079e8212ead4be7863c
Marco Escobar <marcoaescobar@gmail.com>
2018-12-19 18:04:24 -0600

    Check out
    first

---
+++ scratchfile.py
@@ -0,0 +1,23 @@
+"""
+If I really hate pressing `enter` and
+typing all those hash marks, I could
+just do this instead
+"""
+
+# testing
+# commenting
+# in several
+# lines
+
+
+from collections import defaultdict
+
+def get_top_cities(prices):
+    top_cities = defaultdict(int)
+
+    # For each price range
+        # Get city searches in that price
+        # Count num times city was searched
+        # Take top 3 cities & add to dict
+
+    return dict(top_cities)
